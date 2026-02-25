7bc462d3c210d03cce8d626630822c397c370644
Alp Efe Şekerci <alpefesekerci@gmail.com>
2026-02-25 14:04:18 +0300

    Kullanıcıdan veri alan program

---
+++ java_Learning/src/week01/dataProgram.java
@@ -0,0 +1,21 @@
+package week01;
+
+import java.util.Scanner;
+
+public class dataProgram {
+     static void main() {
+         new Scanner(System.in);
+
+         String adSoyad = "Alp"+" "+"Efe"+" "+"Şekerci" ;
+
+         int yas = 24;
+
+         double maas = 5000;
+
+         System.out.println("Adı Soyadı:" + adSoyad);
+         System.out.println("Yaşı:" + yas);
+         System.out.println("Maaşı:" + maas);
+            
+    }
+
+}
