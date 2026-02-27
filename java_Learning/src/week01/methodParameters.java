81a83627812deafe0c65ce5243b01a69462d6d3c
Alp Efe Şekerci <alpefesekerci@gmail.com>
2026-02-27 09:50:24 +0300

    En az 3 adet parametre alan ve değer döndüren method örneği
    Öğrenci adı numarası ve notu listelenen metot

--- java_Learning/src/week01/methodParameters.java
+++ java_Learning/src/week01/methodParameters.java
@@ -1,6 +1,22 @@
 package week01;
 
 public class methodParameters {
+    static String ogrenciSistemi(String ad,int numara,int not) {
+        return "Öğrenci: " + ad + " " + " - No: " + numara + " - Sınav Notu: " + not;
+        }
 
+    static void main() {
+        String[] isimler = {"Alp", "İsmet", "Bora"};
+        int[] numaralar = {2021067, 2021007, 2021034};
+        int[] notlar = {95, 100, 90};
+
+        System.out.println("ÖĞRENCİ NOT LİSTESİ");
+
+        for (int i = 0; i < isimler.length; i++) {
+
+            System.out.println(ogrenciSistemi(isimler[i], numaralar[i], notlar[i]));
+        }
     }
+    }
+
 
