0c6852a91967207e9341043a63b1f40dda4538f7
Alp Efe Şekerci <alpefesekerci@gmail.com>
2026-03-02 14:06:51 +0300

    değişim
    Çalışırken daha basit yöntemi olduğu aklıma geldi

--- java_Learning/src/week01/evenNumbers.java
+++ java_Learning/src/week01/evenNumbers.java
@@ -2,10 +2,10 @@ package week01;
 
 public class evenNumbers {
     static void main() {
-        for (int i = 0; i <= 100; i = i + 2) {
-            if (i == 0) {
-                continue;
-            }
+        for (int i = 2; i <= 100; i = i + 2) {
+            //if (i == 0) {
+               // continue;
+           // }
             System.out.println(i);
         }
     }
