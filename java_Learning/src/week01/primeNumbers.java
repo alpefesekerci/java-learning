14d1968d013c1fc6e7568f58b49d109445f82d50
Alp Efe Şekerci <alpefesekerci@gmail.com>
2026-03-03 09:33:21 +0300

    Asal sayılar bug düzeltme

--- java_Learning/src/week01/primeNumbers.java
+++ java_Learning/src/week01/primeNumbers.java
@@ -7,6 +7,17 @@ public class primeNumbers {
         System.out.print("Sayıyı giriniz : ");
         int sayi = new Scanner(System.in).nextInt();
         boolean hesaplama = true;
+
+        if (sayi==1) {
+            System.out.println(sayi + " " + "Asal Sayı Değildir.");
+            return;
+        }
+
+        if (sayi < 1){
+            System.out.println(sayi + " " + "Geçersiz Sayıdır.");
+            return;
+        }
+
         for (int i=2; i < sayi; i++)
         {
             if (sayi % i == 0)
