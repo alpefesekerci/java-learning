a398b6ab8808686682477054cdc1c3c27872f0b2
Alp Efe Şekerci <alpefesekerci@gmail.com>
2026-03-03 11:13:33 +0300

    input validation

--- java_Learning/src/week01/perfectNumber.java
+++ java_Learning/src/week01/perfectNumber.java
@@ -5,21 +5,39 @@ import java.util.Scanner;
 public class perfectNumber {
     static void main() {
         Scanner scan = new Scanner(System.in);
+        int number = 0;
 
-        System.out.println("Sayı Giriniz:");
-        int number = scan.nextInt();
-        int total = 0;
+        boolean isValidInput = false;
 
-        for (int i = 1; i < number; i++) {
-            if (number % i == 0) {
-                total = total + i;
+        while (!isValidInput) {
+            System.out.println("Lütfen pozitif bir tam sayı giriniz.");
 
+            if (scan.hasNextInt()) {
+                number = scan.nextInt();
+
+                if (number > 0) {
+                    isValidInput = true;
+                } else {
+                    System.out.println("Hata: Girdiğiniz sayı 0 veya negatif olamaz!");
+                }
+            } else {
+                System.out.println("Hata: Lütfen sadece rakam kullanarak geçerli bir tamsayı giriniz!");
+                scan.next();
             }
-        }
-        if (total == number) {
-            System.out.println(number + " " + "Mükemmel sayıdır.");
-        } else {
-            System.out.println(number + " " + "Mükemmel sayı değildir.");
+
+            int total = 0;
+
+            for (int i = 1; i < number; i++) {
+                if (number % i == 0) {
+                    total = total + i;
+
+                }
+            }
+            if (total == number) {
+                System.out.println(number + " " + "Mükemmel sayıdır.");
+            } else {
+                System.out.println(number + " " + "Mükemmel sayı değildir.");
+            }
         }
         scan.close();
     }
