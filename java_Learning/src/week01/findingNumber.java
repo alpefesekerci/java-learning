8e9d1c47ad10dc57bb0b0275f2ac3faba9d44db1
Alp Efe Şekerci <alpefesekerci@gmail.com>
2026-03-03 11:56:35 +0300

    Sayı Bulma örneği

---
+++ java_Learning/src/week01/findingNumber.java
@@ -0,0 +1,27 @@
+package week01;
+
+import java.util.Scanner;
+
+public class findingNumber {
+    static void main() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Bulmak istediğiniz sayıyı giriniz:");
+        int targetNumber = scan.nextInt();
+        int[] numbers = new int[] {1,2,5,7,9,0};
+        boolean isThere = false;
+
+        for (int number : numbers){
+            if (number == targetNumber){
+                isThere = true;
+                break;
+            }
+        }
+        if (isThere){
+            System.out.println("Sayı mevcuttur.");
+        }else{
+            System.out.println("Sayı mevcut değildir.");
+        }
+
+    }
+}
