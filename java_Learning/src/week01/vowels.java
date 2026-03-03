8d28ec8bec88cd556748f5f3d4492b57d73d0bd3
Alp Efe Şekerci <alpefesekerci@gmail.com>
2026-03-03 10:08:54 +0300

    boolean değişkeni ve while döngüsü eklendi

--- java_Learning/src/week01/vowels.java
+++ java_Learning/src/week01/vowels.java
@@ -5,31 +5,36 @@ import java.util.Scanner;
 public class vowels {
     static void main() {
         Scanner scan = new Scanner(System.in);
+        boolean donguDevamEtsin = true;
 
-        System.out.println("Sesli Harf Giriniz:");
-        char harf = scan.next().charAt(0);
+        while (donguDevamEtsin) {
+            System.out.println("Sesli Harf Giriniz:");
+            char harf = scan.next().charAt(0);
 
-        switch (harf) {
-            case 'A': case 'a' :
-            case 'I': case 'ı' :
-            case 'O': case 'o' :
-            case 'U': case 'u' :
-                System.out.println(harf + " " + "Kalın Sesli harftir.");
-                break;
+            switch (harf) {
+                case 'A': case 'a':
+                case 'I': case 'ı':
+                case 'O': case 'o':
+                case 'U': case 'u':
+                    System.out.println(harf + " " + "Kalın Sesli harftir.");
+                    donguDevamEtsin = false;
+                    break;
 
-            case 'E': case 'e' :
-            case 'İ': case 'i' :
-            case 'Ö': case 'ö' :
-            case 'Ü': case 'ü' :
-                System.out.println(harf + " " + "İnce Sesli harftir.");
-                break;
+                case 'E': case 'e':
+                case 'İ': case 'i':
+                case 'Ö': case 'ö':
+                case 'Ü': case 'ü':
+                    System.out.println(harf + " " + "İnce Sesli harftir.");
+                    donguDevamEtsin = false;
+                    break;
 
                 default:
-                System.out.println(harf + " " + "Sesli Harf Değildir. \nLütfen Geçerli Bir Harf Giriniz.");
+                    System.out.println(harf + " " + "Sesli Harf Değildir. \nLütfen Geçerli Bir Harf Giriniz.");
 
+
+            }
 
         }
 
     }
-
 }
